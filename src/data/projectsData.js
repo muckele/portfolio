bf6e2b6cd3b664e58045c47adfbd0e52b85bdd50
Mathew <muckele7@gmail.com>
2024-03-28 15:01:33 -0400

    creates project Data file information

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -4,7 +4,14 @@ const projectsData = [
     description: 'Connect Four, a game created by Howard Wexler, was first sold under the Connect Four trademark by Milton Bradley in 1974. In this game, each player selects a color and takes turns dropping colored tokens into a six-row, seven-column vertically suspended grid. The objective is to be the first player to form a horizontal, vertical, or diagonal line of four of their tokens. The game has become a quintessential American classic and is enjoyed by people of all ages.',
     image: '../assets/Connect Four Image .png',
     repositoryLink: 'https://github.com/muckele/Connect-4',
-    deploymentLink: 'https://matts-connect-four-game.netlify.app/'  
+    deploymentLink: 'https://matts-connect-four-game.netlify.app/',
+    technologiesUsed: [
+      '![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=white)',
+      '![HTML](https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white)',
+      '![CSS](https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white)',
+      '![Git](https://img.shields.io/badge/GIT-E44C30?style=for-the-badge&logo=git&logoColor=white)',
+      '![Github](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)',
+    ]  
 
   },
   {
@@ -12,22 +19,76 @@ const projectsData = [
     description: "Indulge your taste buds in a culinary adventure with Tasty Trove, your go-to destination for delicious recipes that ignite the chef in you! From mouthwatering mains to irresistible desserts, we've curated a collection that promises to elevate your cooking experience. Explore, cook, and savor the joy of creating culinary wonders. Get ready to embark on a flavorsome journey! Happy Cooking! 🍽️✨",
     image: "../assets/Tasty Trove.png",
     repositoryLink: "https://github.com/muckele/tasty-trove",
-    deploymentLink: "https://tasty-trove.fly.dev/"
+    deploymentLink: "https://tasty-trove.fly.dev/",
+    technologiesUsed: [
+      '![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=white)',
+      '![HTML](https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white)',
+      '![CSS](https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white)',
+      '![Git](https://img.shields.io/badge/GIT-E44C30?style=for-the-badge&logo=git&logoColor=white)',
+      '![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)',
+      '![Node.js](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)', 
+      '![MongoDB](https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white)',
+      '![Express](https://img.shields.io/badge/Express.js-404D59?style=for-the-badge)',
+      'Google OAuth',
+      'Passport.js',
+      'Mongoose'
+    ]
   },
   {
     title: "Ride & Fly",
     description: "Ride & FLy is your hassel-free solution to rider and share your way to airport. This is the app where you can share your journey and fun with fellow buddies as well as the expenses. What inspires us to build Ride & Fly is that as avid travelers, we are tired of the stress and expenses of getting to the aitport. So say good bye to the days of costly cab rides and cumbersome luggae struggles. Ride & FLy will address the failed promise from copmanies like Uber and Lyft, where there would be fewer cars on the road, namely fewer carbon emission and fewer car crashes.",
     image: "../assets/ride&fly_app.png",
     repositoryLink: "https://github.com/muckele/ride-and-fly-front-end",
-    deploymentLink: "https://ride-and-fly.netlify.app/"
+    deploymentLink: "https://ride-and-fly.netlify.app/",
+    technologiesUsed: [
+      '![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)',
+      '![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white)',
+      '![React Router](https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white)',
+      '![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)',
+      '![MongoDB](https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white)',
+      '![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)',
+      '![MUI](https://img.shields.io/badge/MUI-%230081CB.svg?style=for-the-badge&logo=mui&logoColor=white)',
+      '![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB)',
+      '![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)',
+      '![Nodemon](https://img.shields.io/badge/NODEMON-%23323330.svg?style=for-the-badge&logo=nodemon&logoColor=%BBDEAD)',
+      '![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)',
+    ]
   },
   {
     title: "House Hunter",
     description: "House Hunter is a revolutionary app designed to streamline the home-buying process for couples and individuals alike. This innovative application allows users to consolidate their property search by importing details and images of homes they are interested in directly from various real estate websites into one centralized platform. By eliminating the need to juggle between different sites or keep track of numerous listings manually, House Hunter offers a seamless and efficient way to organize and review potential homes. With its user-friendly interface, House Hunter simplifies the decision-making process, enabling users to compare properties, features, and prices side by side. Whether you're in the early stages of looking for a new home or narrowing down your final choices, House Hunter is the perfect companion to help you and your loved ones find the home of your dreams.",
-    image: "../assets/ride&fly_app.png",
-    repositoryLink: "https://github.com/muckele/ride-and-fly-front-end",
-    deploymentLink: "https://ride-and-fly.netlify.app/"
-  }
+    image: "../assets/House Hunter.png",
+    repositoryLink: "https://github.com/muckele/house-hunter",
+    deploymentLink: "https://house-hunter.fly.dev/",
+    technologiesUsed: [
+      '![Python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)',
+      '![Django](https://img.shields.io/badge/Django-092E20?style=for-the-badge&logo=django&logoColor=white)',
+      '![Docker](https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white)',
+      '![PostgreSQL](https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white)',
+      '![AWS](https://img.shields.io/badge/Amazon_AWS-232F3E?style=for-the-badge&logo=amazon-aws&logoColor=white)',
+      '![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)',
+      '![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white)',
+      '![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)',
+      '![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)',
+    ]
+  },
+    {
+    title: "Dog Collector",
+    description: "The Dog Collector app is your ultimate companion for managing and enjoying the company of your furry friends. With a user-friendly interface and intuitive features, this app allows you to create and customize your own virtual dog collection, making it perfect for dog lovers of all ages.  Full CRUD of dogs, toys, and feeding schedules to make sure all of your virtual pups are well taken care of.",
+    repositoryLink: "https://github.com/muckele/dog-collector",
+    deploymentLink: "https://dog-collector.fly.dev/"
+    technologiesUsed: [
+            '![Python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)',
+      '![Django](https://img.shields.io/badge/Django-092E20?style=for-the-badge&logo=django&logoColor=white)',
+      '![Docker](https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white)',
+      '![PostgreSQL](https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white)',
+      '![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)',
+      '![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white)',
+      '![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)',
+      '![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)',
+    ]
+  },
+
 ]
 
 export default projectsData
