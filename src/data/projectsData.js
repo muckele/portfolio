01daf9c8c594cd1482164d064f7e5d712f3b8922
Mathew <muckele7@gmail.com>
2024-04-02 16:00:44 -0400

    renders photo of project on project page

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,8 +1,13 @@
+import ConnectFourImage from '../assets/Connect Four Image .png'
+import TastyTroveImage from '../assets/Tasty Trove.png'
+import RideAndFlyImage from '../assets/ride&fly_app.png';
+import HouseHunterImage from '../assets/House Hunter.png'; 
+
 const projectsData = [
   {
     title: 'Connect Four',
     description: 'Connect Four, a game created by Howard Wexler, was first sold under the Connect Four trademark by Milton Bradley in 1974. In this game, each player selects a color and takes turns dropping colored tokens into a six-row, seven-column vertically suspended grid. The objective is to be the first player to form a horizontal, vertical, or diagonal line of four of their tokens. The game has become a quintessential American classic and is enjoyed by people of all ages.',
-    image: '../assets/Connect Four Image .png',
+    image: ConnectFourImage,
     repositoryLink: 'https://github.com/muckele/Connect-4',
     deploymentLink: 'https://matts-connect-four-game.netlify.app/',
     technologiesUsed: [
@@ -17,7 +22,7 @@ const projectsData = [
   {
     title: "Tasty Trove",
     description: "Indulge your taste buds in a culinary adventure with Tasty Trove, your go-to destination for delicious recipes that ignite the chef in you! From mouthwatering mains to irresistible desserts, we've curated a collection that promises to elevate your cooking experience. Explore, cook, and savor the joy of creating culinary wonders. Get ready to embark on a flavorsome journey! Happy Cooking! 🍽️✨",
-    image: "../assets/Tasty Trove.png",
+    image: TastyTroveImage,
     repositoryLink: "https://github.com/muckele/tasty-trove",
     deploymentLink: "https://tasty-trove.fly.dev/",
     technologiesUsed: [
@@ -37,7 +42,7 @@ const projectsData = [
   {
     title: "Ride & Fly",
     description: "Ride & FLy is your hassel-free solution to rider and share your way to airport. This is the app where you can share your journey and fun with fellow buddies as well as the expenses. What inspires us to build Ride & Fly is that as avid travelers, we are tired of the stress and expenses of getting to the aitport. So say good bye to the days of costly cab rides and cumbersome luggae struggles. Ride & FLy will address the failed promise from copmanies like Uber and Lyft, where there would be fewer cars on the road, namely fewer carbon emission and fewer car crashes.",
-    image: "../assets/ride&fly_app.png",
+    image: RideAndFlyImage,
     repositoryLink: "https://github.com/muckele/ride-and-fly-front-end",
     deploymentLink: "https://ride-and-fly.netlify.app/",
     technologiesUsed: [
@@ -57,7 +62,7 @@ const projectsData = [
   {
     title: "House Hunter",
     description: "House Hunter is a revolutionary app designed to streamline the home-buying process for couples and individuals alike. This innovative application allows users to consolidate their property search by importing details and images of homes they are interested in directly from various real estate websites into one centralized platform. By eliminating the need to juggle between different sites or keep track of numerous listings manually, House Hunter offers a seamless and efficient way to organize and review potential homes. With its user-friendly interface, House Hunter simplifies the decision-making process, enabling users to compare properties, features, and prices side by side. Whether you're in the early stages of looking for a new home or narrowing down your final choices, House Hunter is the perfect companion to help you and your loved ones find the home of your dreams.",
-    image: "../assets/House Hunter.png",
+    image: HouseHunterImage,
     repositoryLink: "https://github.com/muckele/house-hunter",
     deploymentLink: "https://house-hunter.fly.dev/",
     technologiesUsed: [
