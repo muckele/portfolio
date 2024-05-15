cb9ca2a23ae6a0f0734e234775263887e6e74c82
Mathew <muckele7@gmail.com>
2024-05-14 23:36:03 -0400

    updates home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,8 @@ function HomePage() {
             <h1>Mathew Uckele</h1>
             <img src={headshotImage} alt="headshot" className="headshot" />
             <h2 className="home-title">Full Stack Software Engineer</h2>
-            <h4>Empowering businesses with adaptable and versatile full-stack solutions, driven by a passion for quality and innovation.</h4>
+            <h4>Hi I'm Mathew Uckele As a versatile and results-driven Full Stack Computer Engineer, I combine a deep understanding of front-end and back-end technologies to deliver innovative and scalable solutions. With a proven track record of successfully navigating complex projects, I excel in designing and implementing robust applications that optimize user experiences and meet business objectives.
+            My proficiency spans various programming languages, frameworks, and databases, allowing me to adapt seamlessly to evolving technological landscapes.</h4>
         </>
     )
 }
